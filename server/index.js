f972b8e90d2e38619392344197bf40f9dab796a2
Kyungju SHIM <roundshoulderim@gmail.com>
2019-11-09 04:42:21 +0900

    Edit cors option

--- server/index.js
+++ server/index.js
@@ -8,22 +8,35 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-var whitelist = ['http://travel-calculator-client.s3-website.ap-northeast-2.amazonaws.com/', 'http://localhost:3000', 'http://travel-calculator-client.s3-website.ap-northeast-2.amazonaws.com', 'http://localhost:3000/']
+var whitelist = ['http://localhost:3000', 'http://travel-calculator-client.s3-website.ap-northeast-2.amazonaws.com'];
 app.use(cors({ credentials: true, origin: whitelist }));
 
-app.all('/*', function (req, res, next) {
-  res.header(
-    'Access-Control-Allow-Origin',
-    'http://travel-calculator-client.s3-website.ap-northeast-2.amazonaws.com, http://localhost:3000'
-  );
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Content-Type, Authorization, Content-Length, X-Requested-With'
-  );
-  console.log(res.getHeaders());
-  next();
+app.use(function (req, res, next) {
+  var allowedOrigins = ['http://localhost:3000', 'http://travel-calculator-client.s3-website.ap-northeast-2.amazonaws.com', 'http://localhost:3000/', 'http://travel-calculator-client.s3-website.ap-northeast-2.amazonaws.com/'];
+  var origin = req.headers.origin;
+  if (allowedOrigins.indexOf(origin) > -1) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  //res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8020');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', true);
+  return next();
 });
+
+// app.all('/*', function (req, res, next) {
+//   res.header(
+//     'Access-Control-Allow-Origin',
+//     'http://travel-calculator-client.s3-website.ap-northeast-2.amazonaws.com'
+//   );
+//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+//   res.header(
+//     'Access-Control-Allow-Headers',
+//     'Content-Type, Authorization, Content-Length, X-Requested-With'
+//   );
+//   console.log(res.getHeaders());
+//   next();
+// });
 
 app.use(bodyParser.json());
 app.use(session({
